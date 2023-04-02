a68dcef8baeb0af84f7469ca321fdbde1ac5b080
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-02 21:50:26 +1000

    component header

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Header.module.css';
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header() {
   return (
     <div>
-        <h1 className={styles.header}>Ignite</h1>
+        <h1 className={styles.header}>
+          <img src={igniteLogo} alt='Logitpo do Ignite' />
+        </h1>
     </div>
   )
 }
