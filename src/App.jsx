07c738a06621d8d2812ea94e98afca7870087dc6
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-02 22:50:31 +1000

    sidebar part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Post } from './Post';
-import { Header } from './componentes/Header';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css'
 
@@ -9,15 +12,20 @@ export function App() {
     <div className="App">
       <Header/>
 
-      <Post
-        author="Glauber Bandeira"
-        content="12 razões para escolher React para seu próximo projeto de desenvolvimento web"
-      />
-      <Post
-        author="Fernando Bandeira"
-        content="React: Simplicidade, Reutilização e Performance em uma só Biblioteca!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar/>
 
+        <main>
+          <Post
+            author="Glauber Bandeira"
+            content="12 razões para escolher React para seu próximo projeto de desenvolvimento web"
+          />
+          <Post
+            author="Fernando Bandeira"
+            content="React: Simplicidade, Reutilização e Performance em uma só Biblioteca!"
+          />
+        </main>
+      </div>
     </div>
   )
 }
