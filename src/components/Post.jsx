365a0e2b3411e86debc3f317effa860cea3c0fdb
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-03 20:03:12 +1000

    component post

---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/glauberbandeira.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Glauber Bandeira</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="03 de Abril às 20:13h" dateTime="2023-04-03 20:13:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉<a href="">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
