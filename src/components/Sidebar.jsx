f7ca0d39812149c4922332facfdcf69b956cee8b
Glauber Bandeira <glauberbandeira_@hotmail.com>
2023-04-03 19:11:51 +1000

    finished sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
 
@@ -11,12 +12,14 @@ export function Sidebar () {
         />
 
         <div className={styles.profile}>
-            <strong>Diego Fernandes</strong>
+          <img className={styles.avatar} src="https://github.com/glauberbandeira.png" />
+            <strong>Glauber Bandeira</strong>
             <span>Web Developer</span>
         </div>
 
         <footer>
             <a href="#">
+            <PencilLine size={20} />
             Editar seu perfil
             </a>
         </footer>
